c4a3e07cbe62a718b22a78c8581333fc87d7aa1a
Barby Kelly <barbykelly1@gmail.com>
2024-06-19 19:23:58 +0000

    Adjust settings

--- bookblog/settings.py
+++ bookblog/settings.py
@@ -24,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uo3bat1ho5k+%rrg1k&4_q$tyt#wif11%9g4gk$+4b@80(ks!2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-barbykelly-blog-i5e54din0m4.ws-eu114.gitpod.io',
-    '.herokuapp.com'
+    "8000-barbykelly-blog-i5e54din0m4.ws-eu114.gitpod.io",
+    ".herokuapp.com"
 ]
 
 
