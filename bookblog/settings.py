2e6f780109db1f483b3d187918a4096af13859d5
Barby Kelly <barbykelly1@gmail.com>
2024-07-05 09:29:26 +0000

    Add templates

--- bookblog/settings.py
+++ bookblog/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-barbykelly-blog-i5e54din0m4.ws-eu114.gitpod.io",
     "8000-barbykelly-blog-i5e54din0m4.ws-us114.gitpod.io",
     "8000-barbykelly-blog-i5e54din0m4.ws-eu115.gitpod.io",
+    "8000-barbykelly-blog-i5e54din0m4.ws-us115.gitpod.io",
     ".herokuapp.com"
 ]
 
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'bookblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
