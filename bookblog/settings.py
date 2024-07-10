cff1979ac57f8b4e1c402e4dd7206fef1c1c3f60
Barby Kelly <barbykelly1@gmail.com>
2024-07-10 17:12:01 +0000

    Create About app

--- bookblog/settings.py
+++ bookblog/settings.py
@@ -47,8 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote',
     'bkblog',
+    'django_summernote',
+    'about',  
 ]
 
 MIDDLEWARE = [
