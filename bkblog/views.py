d5cb41b6b2bb8b2026f2304ab818c1a45538728a
Barby Kelly <barbykelly1@gmail.com>
2024-07-13 23:14:45 +0000

    Adding comments

--- bkblog/views.py
+++ bkblog/views.py
@@ -26,10 +26,15 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "bkblog/post_detail.html",
-        {"post": post,
-        "coder": "Barby Kelly"},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
