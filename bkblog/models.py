dcb718b35187ef8709ae6b610286e2fc9dd0c345
Barby Kelly <barbykelly1@gmail.com>
2024-07-15 00:54:31 +0000

    Tidy up: edit wording, change the order of the blog articles, delete posts meant for walk-through.

--- bkblog/models.py
+++ bkblog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
