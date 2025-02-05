90bd8b60fcfdbf33fae7daeecdac5efd23cc76b8
BarbyKelly <barbykelly1@gmail.com>
2025-02-05 19:29:50 +0000

    Brought project over to vs code

--- bookblog/settings.py
+++ bookblog/settings.py
@@ -28,12 +28,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "8000-barbykelly-blog-6dmo4lxn54e.ws.codeinstitute-ide.net",
-    "8000-barbykelly-blog-3ot2qy61ffm.ws-eu117.gitpod.io",
-    ".herokuapp.com"
+    "127.0.0.1", ".herokuapp.com"
 ]
 
 
